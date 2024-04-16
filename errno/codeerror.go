f1d78b0bc9663a23d444ee24db8fcdedb52b8f27
liuchaowei <liuchaowei@sensetime.com>
2024-04-16 15:39:30 +0800

    fix

--- errno/codeerror.go
+++ errno/codeerror.go
@@ -9,15 +9,15 @@ import (
 type BaseResp struct {
 	Code int    `json:"code"`
 	Msg  string `json:"message"`
-	Desc string `json:"desc,omitempty"`
+	Desc string `json:"-"`
 	Data any    `json:"data,omitempty"`
 }
 
-type ErrorResp struct {
-	Code int    `json:"code"`
-	Msg  string `json:"message"`
-	Desc string `json:"desc,omitempty"`
-}
+// type ErrorResp struct {
+// 	Code int    `json:"code"`
+// 	Msg  string `json:"message"`
+// 	Desc string `json:"-"`
+// }
 
 func (e *BaseResp) Error() string {
 	return fmt.Sprintf("%s:%s", e.Msg, e.Desc)
@@ -27,11 +27,12 @@ var ErrorHandler = func(ctx context.Context, err error) (int, any) {
 
 	e, ok := err.(*BaseResp)
 	if ok {
-		return 200, &ErrorResp{
-			Code: e.Code,
-			Msg:  e.Msg,
-			// Desc: e.Desc,
-		}
+		// return 200, &ErrorResp{
+		// 	Code: e.Code,
+		// 	Msg:  e.Msg,
+		// 	// Desc: e.Desc,
+		// }
+		return 200, e
 	}
 	return 200, defaultError
 }
@@ -39,7 +40,7 @@ var ErrorHandler = func(ctx context.Context, err error) (int, any) {
 var OkHandler = func(ctx context.Context, data any) any {
 
 	if isEmpty(data) {
-		data = nil //make([]string, 0)
+		data = nil
 	}
 
 	return &BaseResp{
